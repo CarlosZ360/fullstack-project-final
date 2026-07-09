cd50b2a20a222823240d8f856086b6276ae36dfc
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2026-07-09 15:40:40 -0400

    test: aplicar incidente para simulacro de pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import Header from "./componentes/Header";
 import TaskInput from "./componentes/TaskInput";
-import Tasklist from "./componentes/Tasklist";
 import Footer from "./componentes/Footer";
 const API_URL = import.meta.env.VITE_API_URL;
 
